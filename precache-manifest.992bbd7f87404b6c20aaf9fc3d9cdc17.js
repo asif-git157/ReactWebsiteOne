3eebb8be3b1eb9ee4e084a8844ead9cc31cb396b
Asif <asif.ali@purplecommerce.com>
2023-03-22 21:20:48 +0530

    Updates

---
+++ precache-manifest.992bbd7f87404b6c20aaf9fc3d9cdc17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32983542e5e12c3af3b0",
+    "url": "/ReactWebsiteOne/static/css/main.d29cdd86.chunk.css"
+  },
+  {
+    "revision": "32983542e5e12c3af3b0",
+    "url": "/ReactWebsiteOne/static/js/main.d96294c9.chunk.js"
+  },
+  {
+    "revision": "0185a0e9180b7d78034a",
+    "url": "/ReactWebsiteOne/static/js/runtime~main.23be95ea.js"
+  },
+  {
+    "revision": "4936670b3682cffce817",
+    "url": "/ReactWebsiteOne/static/js/2.3f04e7eb.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/ReactWebsiteOne/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "5793a3705ee4c8fe618529f312d68888",
+    "url": "/ReactWebsiteOne/index.html"
+  }
+];
